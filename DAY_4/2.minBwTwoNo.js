1e7a509386c4dddd7669f185509745aefdd59514
Megha <developermegha98@gmail.com>
2023-01-29 18:34:29 +0530

    minBtwTwoNo

--- DAY_4/2.minBwTwoNo.js
+++ DAY_4/2.minBwTwoNo.js
@@ -1,15 +1,15 @@
-// function minBwTwoNo(num1, num2) {
-//   if (num1 < num2) {
-//     return num1;
-//   } else {
-//     return num2;
-//   }
-// }
+function minBwTwoNo(num1, num2) {
+  if (num1 < num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
 
-// let number1 = 23;
-// let number2 = 45;
-// let answer = minBwTwoNo(number1, number2);
-// console.log(`min bw ${number1} and ${number2} is - `, answer);
+let number1 = 23;
+let number2 = 45;
+let answer = minBwTwoNo(number1, number2);
+console.log(`min bw ${number1} and ${number2} is - `, answer);
 
 let minBwTwoNo = (num1, num2) => {
   if (num1 < num2) {
